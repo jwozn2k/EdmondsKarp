fcd1722f354a72613c3e7f0d404fc0b15b6bafad
Jakub Woźniak <jakubwozniak@student.agh.edu.pl>
2021-05-24 19:38:13 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+from utils import read_from_the_file
+
+if __name__ == "__main__":
+    capacity = read_from_the_file()
+    print("Hello world!")
---
+++ utils.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+def read_from_the_file():
+    pass
